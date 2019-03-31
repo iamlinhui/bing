314cf5c3a5ad8a23a0c95f1ae147bc114bd77d51
linhui <linhui@damailicai.com>
2019-03-31 19:59:18 +0800

    #fix cache

--- src/main/java/cn/promptness/bing/schedule/ImageSchedule.java
+++ src/main/java/cn/promptness/bing/schedule/ImageSchedule.java
@@ -1,16 +1,10 @@
 package cn.promptness.bing.schedule;
 
-import cn.promptness.bing.config.QiniuProperties;
 import cn.promptness.bing.pojo.ImageDO;
 import cn.promptness.bing.service.ImageService;
 import cn.promptness.bing.utlis.ImageUtils;
+import cn.promptness.bing.utlis.QiniuUtils;
 import cn.promptness.bing.vo.ImageVO;
-import com.qiniu.common.QiniuException;
-import com.qiniu.http.Response;
-import com.qiniu.storage.UploadManager;
-import com.qiniu.util.Auth;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -25,18 +19,13 @@ import java.util.Date;
 @Component
 public class ImageSchedule {
 
-    private Logger logger = LoggerFactory.getLogger(ImageSchedule.class);
 
-    @Autowired
-    private QiniuProperties qiniuProperties;
-    @Autowired
-    private Auth auth;
-    @Autowired
-    private UploadManager uploadManager;
     @Autowired
     private ImageUtils imageUtils;
     @Autowired
     private ImageService imageService;
+    @Autowired
+    private QiniuUtils qiniuUtils;
 
 
     @Scheduled(cron = "59 0 0 * * ?")
@@ -61,7 +50,7 @@ public class ImageSchedule {
         }
 
         String fileName = imageUtils.getFileName();
-        boolean upload = upload(file, fileName);
+        boolean upload = qiniuUtils.upload(file, fileName);
 
         if (upload) {
             ImageDO imageDO = new ImageDO();
@@ -73,23 +62,5 @@ public class ImageSchedule {
 
     }
 
-    /**
-     * 简单上传，使用默认策略，只需要设置上传的空间名就可以了
-     */
-    private String getUpToken() {
-        return auth.uploadToken(qiniuProperties.getBucketName());
-    }
 
-    private boolean upload(File path, String filename) {
-        try {
-            // 调用put方法上传
-            Response res = uploadManager.put(path, filename, getUpToken());
-            logger.info(res.bodyString());
-            return true;
-        } catch (QiniuException e) {
-            Response r = e.response;
-            logger.error(r.toString());
-        }
-        return false;
-    }
 }
